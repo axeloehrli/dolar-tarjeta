9bc46f4ed4a6278493540b458ca6d1848838ec6f
axeloehrli <axel.oehrli@gmail.com>
2022-07-27 05:02:55 -0300

    Added menu icon to header component

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
+import Image from "next/image"
 import styles from "../styles/Header.module.css"
-
+import MenuIcon from "../public/menu.svg"
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.headerWrapper}>
         <h2 className={styles.h2}>Dólar tarjeta</h2>
+        <Image width={30} height={30} src={MenuIcon}  alt="menu"/>
       </div>
     </header>
   )
