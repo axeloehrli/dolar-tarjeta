b24d68d40aca40d0b681cfcb946b3629b4a153b6
axeloehrli <axel.oehrli@gmail.com>
2022-10-02 09:00:23 -0300

    Changed favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,10 @@ export default function Document() {
   return (
     <Html lang='es'>
       <Head>
-        <link rel="shortcut icon" href="/favicon_io/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127654850827260"
           crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
