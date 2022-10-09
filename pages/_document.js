e4f24a85087615d572045a507e82e46ef5d9ddc8
axeloehrli <axel.oehrli@gmail.com>
2022-10-09 15:38:38 -0300

    Added newrelic client side support

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,14 @@ export default function Document() {
   return (
     <Html lang='es'>
       <Head>
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127654850827260"
+          crossOrigin="anonymous"></script>
+        <script async type="text/javascript" src="/newrelic-meta.js" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127654850827260"
-          crossOrigin="anonymous"></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;400;500;800&display=swap" rel="stylesheet" />
